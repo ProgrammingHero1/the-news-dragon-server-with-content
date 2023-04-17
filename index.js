1b7f13709f29b0f1a25350e8797e9cf8be8b4a08
Programming Hero <programming.hero1@gmail.com>
2023-04-17 18:42:39 -0500

    added news data and created news api

--- index.js
+++ index.js
@@ -4,16 +4,38 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 app.use(cors());
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.send('Dragon is running')
 });
 
-app.get('/categories', (req, res) =>{
-    console.log(categories);
+app.get('/categories', (req, res) => {
     res.send(categories);
+})
+
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews)
+})
+
+app.get('/categories/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    if (id === 0) {
+        res.send(news)
+    }
+    else {
+        const categoryNews = news.filter(n => parseInt(n.category_id) === id);
+        res.send(categoryNews)
+    }
+
 })
 
 app.listen(port, () => {
